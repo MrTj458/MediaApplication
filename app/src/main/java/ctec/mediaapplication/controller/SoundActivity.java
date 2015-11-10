10fab7c79fb34298fb240b95093cfcff99cad7f5
Trevor <thod0127@740s-pl-130.local>
2015-11-10 13:53:27 -0700

    Finished the sound activity

--- app/src/main/java/ctec/mediaapplication/controller/SoundActivity.java
+++ app/src/main/java/ctec/mediaapplication/controller/SoundActivity.java
@@ -1,15 +1,126 @@
 package ctec.mediaapplication.controller;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.*;
+import android.content.Intent;
+import android.view.View;
+import android.media.MediaPlayer;
 
-public class SoundActivity extends AppCompatActivity
+public class SoundActivity extends Activity implements Runnable
 {
+    private Button startButton;
+    private Button stopButton;
+    private Button pauseButton;
+    private Button videoButton;
+    private MediaPlayer soundPlayer;
+    private SeekBar soundSeekBar;
+    private Thread soundThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sound);
+
+        startButton = (Button) findViewById(R.id.playButton);
+        pauseButton = (Button) findViewById(R.id.pauseButton);
+        stopButton = (Button) findViewById(R.id.stopButton);
+        soundSeekBar = (SeekBar) findViewById(R.id.seekBar);
+        videoButton = (Button) findViewById(R.id.toVideoButton);
+        soundPlayer = MediaPlayer.create(this.getBaseContext(), R.raw.alanwalker_force);
+
+        setupListeners();
+
+        soundThread = new Thread(this);
+        soundThread.start();
+    }
+
+    private void setupListeners()
+    {
+        startButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                soundPlayer.start();
+            }
+        });
+
+        pauseButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                soundPlayer.pause();
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                soundPlayer.stop();
+                soundPlayer = MediaPlayer.create(getBaseContext(), R.raw.alanwalker_force);
+            }
+        });
+
+        videoButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View currentView)
+            {
+                Intent myIntent = new Intent(currentView.getContext(), VideoActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+
+        soundSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+        {
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar)
+            {
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar)
+            {
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+            {
+                if (fromUser)
+                {
+                    soundPlayer.seekTo(progress);
+                }
+            }
+        });
+    }
+
+    public void run()
+    {
+        int currentPosition = 0;
+        int soundTotal = soundPlayer.getDuration();
+        soundSeekBar.setMax(soundTotal);
+
+        while(soundPlayer != null && currentPosition < soundTotal)
+        {
+            try
+            {
+                Thread.sleep(300);
+                currentPosition = soundPlayer.getCurrentPosition();
+            }
+            catch(InterruptedException soundException)
+            {
+                return;
+            }
+            catch(Exception otherException)
+            {
+                return;
+            }
+            soundSeekBar.setProgress(currentPosition);
+        }
     }
 }
